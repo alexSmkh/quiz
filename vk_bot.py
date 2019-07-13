db8acdcec63c7437851c45c1d91df28f70b92d51
alexSmkh <alexshikari9333@gmail.com>
2019-07-14 02:07:13 +0300

    Changed the keys for vk players

--- vk_bot.py
+++ vk_bot.py
@@ -38,7 +38,7 @@ def start_chatting(event, vk):
 
 
 def handle_new_question_request(event, vk):
-    key_for_player_question_ids = f'vk:player_{event.user_id}:question_ids'
+    key_for_player_question_ids = f'player_vk_{event.user_id}:question_ids'
     question_and_answer = get_question_and_answer(
         redis_obj,
         key_for_player_question_ids
@@ -51,7 +51,7 @@ def handle_new_question_request(event, vk):
 
 
 def handle_give_up(event, vk):
-    key_for_player_question_ids = f'vk:player_{event.user_id}:question_ids'
+    key_for_player_question_ids = f'player_vk{event.user_id}:question_ids'
     last_player_question = get_last_player_question(
         redis_obj,
         key_for_player_question_ids
@@ -73,7 +73,7 @@ def handle_give_up(event, vk):
 
 
 def handle_solution_attempt(event, vk):
-    key_for_player_question_ids = f'vk:player_{event.user_id}:question_ids'
+    key_for_player_question_ids = f'player_vk{event.user_id}:question_ids'
     result = check_player_answer(
         redis_obj,
         event.text,
